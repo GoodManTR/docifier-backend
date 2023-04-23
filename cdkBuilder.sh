124921bf2043bdd8db81a6be92cc50602111d0a9
bahadırKurul <104140593+GoodManTR@users.noreply.github.com>
2023-04-23 07:05:41 +0300

    init

--- cdkBuilder.sh
+++ cdkBuilder.sh
@@ -8,16 +8,16 @@ npm run build
 npm run copy-templates
 npm run create-layer
 
-export AWS_GATEWAY_CERTIFICATE_ARN="arn:aws:acm:eu-west-1:729211218471:certificate/086b379c-2186-4932-b609-0dc666540092"
-export AWS_API_CERTIFICATE_ARN="arn:aws:acm:us-east-1:729211218471:certificate/8910e0ab-9f3e-4978-8609-8721c7e53b49"
+export AWS_GATEWAY_CERTIFICATE_ARN="arn:aws:acm:eu-west-1:959503346295:certificate/b5b26d31-12a9-4f25-a116-9863e27b0cc5"
+export AWS_API_CERTIFICATE_ARN="arn:aws:acm:us-east-1:959503346295:certificate/16ac64c7-2f97-4328-a19f-af9042cd4bfb"
 export AWS_API_TYPE=""
 export AWS_REGION="eu-west-1"
-export AWS_DOMAIN='api.ihsanceyiz.com'
+export AWS_DOMAIN='api.goodmanio.net'
 
-# npm run cdk -- -a "ts-node ./cdk/GoodManStack.ts" synth --context @aws-cdk/core:stackRelativeExports="false" --context @aws-cdk/core:newStyleStackSynthesis="true" --context @aws-cdk/core:bootstrapQualifier="hnb659fds" GoodManStack -q --profile bahadir
+# npm run cdk -- -a "ts-node ./cdk/GoodManStack.ts" synth --context @aws-cdk/core:stackRelativeExports="false" --context @aws-cdk/core:newStyleStackSynthesis="true" --context @aws-cdk/core:bootstrapQualifier="hnb659fds" GoodManStack -q --profile docifier-bahadir
 # echo "synth finished"
-# npm run cdk -- bootstrap --toolkit-stack-name CDKToolkit --qualifier hnb659fds --profile bahadir
+# npm run cdk -- bootstrap --toolkit-stack-name CDKToolkit --qualifier hnb659fds --profile docifier-bahadir
 # echo "bootstrap finished"
 
-npm run cdk -- deploy --require-approval never GoodManStack --profile bahadir
+npm run cdk -- deploy --require-approval never GoodManStack --profile docifier-bahadir
 echo "deploy finished"
